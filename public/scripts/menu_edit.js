3b3be7d67109399ef32c591d66d6d4ca3c66424d
Brandon Goodman <bran99@gmail.com>
2015-06-23 15:48:46 -0400

    Added button functionality.  Need to add repositioning logic on button

--- public/scripts/menu_edit.js
+++ public/scripts/menu_edit.js
@@ -2,28 +2,26 @@ var addRow = $('#addRow');
 console.log(addRow);
 $('#addRow').on('click', function () {
   console.log('made it here...');
-  var lastDiv = $('div');
-  console.log(lastDiv);
 
-  var newDiv = $('<div>'),
+  var newDiv = $('<div class="pure-control-group">'),
   newName = $('<input name="orders[name][]" type="text" placeholder="Name">'),
   newDesc = $('<input name="orders[description][]" type="text" placeholder="Description">'),
   newPrice = $('<input name="orders[price][]" type="number" placeholder="Price">'),
   newSelect = $('<select name="orders[type][]" class="type">')
-  newAppetizer= $('<option value="Appetizer">'),
-  newEntree = $('<option value="Entree">'),
-  newDessert = $('<option value="Dessert">'),
-  newOther = $('<option value="Other">');
+  newAppetizer= $('<option value="Appetizer">').text('Appetizer'),
+  newEntree = $('<option value="Entree">').text('Entree'),
+  newDessert = $('<option value="Dessert">').text('Dessert'),
+  newOther = $('<option value="Other">').text('Other');
 
   newSelect.append(newAppetizer)
            .append(newEntree)
            .append(newDessert)
-           .append(newjOther);
+           .append(newOther);
 
   newDiv.append(newName)
         .append(newDesc)
         .append(newPrice)
         .append(newSelect);
 
-  lastDiv.append(newDiv);
+  $('ul').append($('<li>').append(newDiv));
 });
