edecbbd69689038313097ceeee8210aec3dca47c
Brandon Goodman <bran99@gmail.com>
2015-06-24 10:07:33 -0400

    Orders functional.  Need styling, order button, and reworking into form

--- public/scripts/orders.js
+++ public/scripts/orders.js
@@ -1,8 +1,8 @@
 var total = 0;
 $('li').on('click', function () {
-  console.log($('this small'));
+  console.log($(this).find('small'));
   var newOrderItem = $(this).clone();
-  var priceAdd = parseInt($('this small').html());
+  var priceAdd = parseInt($(this).find('small').text());
   total += priceAdd;
   console.log(priceAdd);
   $('#total').text('total: ' + total);
