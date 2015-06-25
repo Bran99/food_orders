dfe6dcb6ce181a569261cc2f7d2c2b247b5e28de
Brandon Goodman <bran99@gmail.com>
2015-06-25 08:53:45 -0400

    Fixed show.ejs

--- public/scripts/orders.js
+++ public/scripts/orders.js
@@ -4,13 +4,12 @@ var $orderForm = $('.current-order');
 
 $('label').on('click', function () {
   var $input = $('#' + $(this).attr('for'));
-  console.log($input);
 
   var attrs = ["name", "type", "description", "price"];
 
   attrs.forEach(function (att, i) {
     var $newInput = $("<input type='hidden'>");
-    $newInput.attr("name", "orders[" + orderCount + "][" + att + "]");
+    $newInput.attr("name", "orders[items][" + orderCount + "][" + att + "]");
     $newInput.attr("value", $input.data(att));
 
     $orderForm.prepend($newInput);
@@ -18,12 +17,10 @@ $('label').on('click', function () {
 
   orderCount++;
 
-  console.log($(this).find('small'));
   var newOrderItem = $(this).clone();
   newOrderItem.prepend($('<br/>'))
   var priceAdd = parseInt($(this).find('small').html());
   total += priceAdd;
-  console.log(priceAdd);
   $('#total').attr('value', total);
   $('#totalHere').text(total);
   $('.current-order').prepend(newOrderItem);
